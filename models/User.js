4fcb42295d64305f91ecf8c217be93fbe2d65c59
Oleksandr Zhynkin <godinsighted@gmail.com>
2024-09-22 20:36:33 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -14,7 +14,7 @@ const userSchema = new Schema(
       required: [true, "Email is required"],
     },
     dayOfBirth: {
-      type: Date,
+      type: String,
       required: true,
     },
     source: {
